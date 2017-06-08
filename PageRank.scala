db556309dbd3f38e17c0f27e3d5fcac6331cea8f
Piyush Chawla <chawla.piyush4650@gmail.com>
2017-06-08 16:05:19 +0530

    final tripleOps file
    File to run on the sever added

---
+++ PageRank.scala
@@ -0,0 +1,62 @@
+package net.sansa_stack.examples.spark.rdf
+
+import net.sansa_stack.rdf.spark.io.NTripleReader
+import net.sansa_stack.rdf.spark.model.JenaSparkGraphXOps
+import org.apache.spark.sql.SparkSession
+import java.net.{URI => JavaURI}
+
+
+import scala.collection.mutable
+
+/*
+ * Computes the PageRank of Resources from an input .nt file.
+ */
+object PageRank {
+
+  def main(args: Array[String]) = {
+//    if (args.length < 1) {
+//      System.err.println(
+//        "Usage: Resource PageRank <input>")
+//      System.exit(1)
+//    }
+    val input = "/home/mypc/SANSA-Examples/sansa-examples-spark/target/classes/rdf.nt" //arg(0)
+    val optionsList = args.drop(1).map { arg =>
+      arg.dropWhile(_ == '-').split('=') match {
+        case Array(opt, v) => (opt -> v)
+        case _             => throw new IllegalArgumentException("Invalid argument: " + arg)
+      }
+    }
+    val options = mutable.Map(optionsList: _*)
+
+    options.foreach {
+      case (opt, _) => throw new IllegalArgumentException("Invalid option: " + opt)
+    }
+    println("======================================")
+    println("|   PageRank of resources example    |   RDF.nt")
+    println("======================================")
+
+    val sparkSession = SparkSession.builder
+      .master("local[*]")
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .config("spark.kryo.registrator", "net.sansa_stack.rdf.spark.io.JenaKryoRegistrator")
+      .appName("Resource PageRank example (" + input + ")")
+      .getOrCreate()
+
+    val ops = JenaSparkGraphXOps(sparkSession.sparkContext)
+    import ops._
+
+    val triplesRDD = NTripleReader.load(sparkSession, JavaURI.create(input))
+
+    val graph = makeGraph(triplesRDD)
+    val pagerank = graph.pageRank(0.00001).vertices
+//    val report = pagerank.join(graph.vertices)
+//      .map({ case (k, (r, v)) => (r, v, k) })
+//      .sortBy(50 - _._1)
+//
+//    report.take(50).foreach(println)
+
+    sparkSession.stop
+
+  }
+
+}
